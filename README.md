7e1adc755aa5a369208061b543338dd11a0e3bbb
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2022-11-04 21:44:54 +0200

    make /analyze return stressAverage

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+uvicorn main:app --reload
 ```
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import joblib
+import numpy as np
 
-class SentenceRequest(BaseModel):
-    sentence: str
+model = joblib.load('model.pkl')
+cv = joblib.load('cv.pkl')
+
+class RequestPayload(BaseModel):
+    messageList: list[str]
 
 app = FastAPI()
 
 @app.post("/analyze")
-async def analyze(request: SentenceRequest):
-    return {"ret": request.sentence}
+async def analyze(request: RequestPayload): 
+    if not len(request.messageList):
+        raise HTTPException(status_code=422, detail="Empty list in request payload")
+    results = model.predict(cv.transform(request.messageList).toarray())
+    stressAverage = float(np.average(results))
+    return {"stressAverage": stressAverage}
