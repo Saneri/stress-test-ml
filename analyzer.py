276b50d047058ae352b909b12d8eb1b4a4ff4f5f
PatrikHolopainen <oncerecursive@gmail.com>
2022-11-05 14:57:01 +0200

    Changed return type from list to object

--- analyzer.py
+++ analyzer.py
@@ -6,4 +6,4 @@ cv = joblib.load('pkl/cv.pkl')
 def analyzeStress(wordList):
     results = model.predict(cv.transform(wordList).toarray())
     resultsAsInt = [int(x) for x in results]
-    return resultsAsInt
+    return {"messageCount": len(resultsAsInt),"stressMessages":sum(resultsAsInt)}
